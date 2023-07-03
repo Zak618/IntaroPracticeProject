fd514bd3788c62ec88e56fbbcf3f5b7e922e7818
Egorova Maria <mashaegorova2002@gmail.com>
2023-07-03 22:25:09 +0300

    feat: добавлены сущности и миграции

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ff98a6a459fbe39f9111078efaf063b67fd0e53c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ff98a6a459fbe39f9111078efaf063b67fd0e53c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
