676baf8efd155624071826dccda891266e3ecf35
Maria <mit.maria2019@yandex.ru>
2023-07-06 22:33:14 +0300

    feat: возвращается список категорий, генерация данных для шапки

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StoreController extends AbstractController
+class StoreController extends BaseController
 {
     #[Route('', name: 'app_store')]
     public function index(): Response
     {
+        dd($this->getHeader());
         return $this->render('store/index.html.twig', [
             'controller_name' => 'StoreController',
             'title' => 'djf'
