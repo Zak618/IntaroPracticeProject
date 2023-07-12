4842050774875833540259da3a4ba72e76cf8a29
Rolan <rolan@mail.ru>
2023-07-12 22:12:43 +0300

    Добавил таймик на alert

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,3 +1,73 @@
+// function addToCart(offerId) {
+//     fetch(`/product/${offerId}`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ offerid: offerId }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         alert(data);
+//         if (data.status === 'success') {
+//           alert('Товар успешно добавлен в корзину');
+//         } else {
+//             alert(data.message);
+//         }
+//       })
+//       .catch(error => {
+//         console.log('Произошла ошибка:', error);
+//       });
+//   }
+  
+//   function decreaseQuantity(offerId) {
+//     fetch(`/product/${offerId}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ offerid: offerId }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         if (data.status === 'success') {
+//           // TODO нужно показывать https://getbootstrap.ru/docs/5.1/components/alerts/
+//           // красивые уведомленьки внизу где-нибудь
+//           // с таймером, чтобы потом они пропадали
+//             alert('Количество товара успешно уменьшено');
+//         } else {
+//             alert(data.message);
+//         }
+//       })
+//       .catch(error => {
+//         alert('Произошла ошибка:', error);
+//       });
+//   }
+  
+//   function removeFromCart(offerId) {
+//     fetch(`/product/${offerId}/all`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ offerid: offerId }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         if (data.status === 'success') {
+//             alert('Оффер успешно удален из корзины');
+//         } else {
+//             alert(data.message);
+//         }
+//       })
+//       .catch(error => {
+//         alert('Произошла ошибка:', error);
+//       });
+//   }
+  
+
+
 function addToCart(offerId) {
     fetch(`/product/${offerId}`, {
       method: 'POST',
@@ -10,16 +80,19 @@ function addToCart(offerId) {
       .then(data => {
         alert(data);
         if (data.status === 'success') {
-          alert('Товар успешно добавлен в корзину');
+          setTimeout(function() {
+            alert('Товар успешно добавлен в корзину');
+          }, 3000); // таймаут на 3 секунды
         } else {
+          setTimeout(function() {
             alert(data.message);
+          }, 3000); // таймаут на 3 секунды
         }
       })
       .catch(error => {
         console.log('Произошла ошибка:', error);
       });
   }
-  
   function decreaseQuantity(offerId) {
     fetch(`/product/${offerId}`, {
       method: 'DELETE',
@@ -30,20 +103,21 @@ function addToCart(offerId) {
     })
       .then(response => response.json())
       .then(data => {
+        console.log(data);
         if (data.status === 'success') {
-          // TODO нужно показывать https://getbootstrap.ru/docs/5.1/components/alerts/
-          // красивые уведомленьки внизу где-нибудь
-          // с таймером, чтобы потом они пропадали
+          setTimeout(function() {
             alert('Количество товара успешно уменьшено');
+          }, 3000); // таймаут на 3 секунды
         } else {
+          setTimeout(function() {
             alert(data.message);
+          }, 3000); // таймаут на 3 секунды
         }
       })
       .catch(error => {
         alert('Произошла ошибка:', error);
       });
   }
-  
   function removeFromCart(offerId) {
     fetch(`/product/${offerId}/all`, {
       method: 'DELETE',
@@ -56,9 +130,13 @@ function addToCart(offerId) {
       .then(data => {
         console.log(data);
         if (data.status === 'success') {
+          setTimeout(function() {
             alert('Оффер успешно удален из корзины');
+          }, 3000); // таймаут на 3 секунды
         } else {
+          setTimeout(function() {
             alert(data.message);
+          }, 3000); // таймаут на 3 секунды
         }
       })
       .catch(error => {
